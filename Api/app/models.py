19af6fb811731dbe1a76e0a03d2871cdad1b47c7
aivilo-adamah <adamaholivia444@gmail.com>
2023-06-08 02:32:32 +0200

    First API Django RestFul

---
+++ Api/app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+User = get_user_model()
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ Api/app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Post
+from .serializers import PostSerializer
+# Create your views here.
+
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
